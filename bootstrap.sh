335eea22a8a84505bc7658b561e616c1b0829f1d
Ferrari <shiyung@gmail.com>
2013-08-22 02:04:50 +0800

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,71 @@
-#!/bin/env sh
+#!/usr/bin/env bash
 
 endpath="$HOME/.ferrari"
+backuppath="$endpath/bak"
+bundlepath="$endpath/.vim/bundle"
+checkfiles=('.vim' '.vimrc' '.gvimrc' '.bashrc' '.tmux-conf')
 
-warn() {
-    echo "$1" >&2
+msg() {
+  printf '%b\n' "$1" >&2
 }
 
-die() {
-    warn "$1"
-    exit 1
+success() {
+  if [ "$ret" -eq '0' ]; then
+    msg "\e[32m[✔]\e[0m ${1}${2}"
+  fi
 }
+
+error() {
+  msg "\e[31m[✘]\e[0m ${1}${2}"
+  exit 1
+}
+
+do_backup() {
+  today=`date +%Y%m%d_%s`
+  for i in ${checkfiles[@]}; do
+    source="$HOME/$i"
+    filename=`echo $i | sed 's/\.//g'`
+    [ -e $source ] && [ ! -L "$source" ] && mv $source "$backuppath/$filename.$today"
+  done
+  ret="$?"
+  success "$1"
+}
+
+lnif() {
+  if [ -e "$1" ]; then
+    ln -sf "$1" "$2"
+  fi
+  ret="$?"
+}
+
+create_symlinks() {
+  lnif "$endpath/vimrc" "$HOME/.vimrc"
+  lnif "$endpath/tmux.conf" "$HOME/.tmux.conf"
+  lnif "$endpath/bashrc" "$HOME/.bashrc"
+  lnif "$endpath/.vim" "$HOME/.vim"
+
+  ret="$?"
+  success "$1"
+}
+
+# Setup Directory
+msg "Setup personal dotfiles..."
+mkdir -p $bundlepath
+mkdir -p $backuppath
 
 # Backup existing .vim stuff
-echo "backing up current vim config\n"
-today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
+msg "backing up current dotfiles..."
+do_backup
 
+# Origin Project is spf13-vim.git
+msg "cloning dotfiles repository..."
+git clone --recursive https://github.com/Ferrari/Personal-Vim.git $endpath
+create_symlinks
 
-echo "cloning ferrari\n"
-#git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git $endpath
-git clone --recursive git@github.com:Ferrari/Personal-Vim.git $endpath
-mkdir -p $endpath/.vim/bundle
-ln -s $endpath/vimrc $HOME/.vimrc
-ln -s $endpath/.vim $HOME/.vim
+msg "Installing Vundle"
+if [ ! -d "$bundlepath/vundle" ]; then
+  git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
 
-echo "Installing Vundle"
-git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-
-echo "installing plugins using Vundle"
+msg "installing plugins using Vundle"
 vim +BundleInstall! +BundleClean +q
